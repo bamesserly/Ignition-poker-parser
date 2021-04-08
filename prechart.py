654771ed24c5dcdc64e0e2bcfcfa26009b1cbee5
Ben Messerly <bamesserly@gmail.com>
2021-04-08 00:40:46 -0500

    Data concatenated, working on plotting.
    Not functional.

--- prechart.py
+++ prechart.py
@@ -108,15 +108,150 @@ def PlotHands(df, weights=None, tag=None):
     plt.show()
     f.savefig("{}.png".format(tag))
 
+def IsSuited(row):
+    card1 = row['card1'].strip("' ")
+    card2 = row['card2'].strip("' ")
+    c1, s1 = card1[0].upper(), card1[1].lower()
+    c2, s2 = card2[0].upper(), card2[1].lower()
+    if s1 == s2:
+        return True
+    else:
+        return False
+
+# Cards ordered without suits, e.g. "AA", "J3"
+def BareCards(row):
+    c1 = row["card1"].strip("' ")
+    c1 = c1[0].upper()
+    c2 = row["card2"].strip("' ")
+    c2 = c2[0].upper()
+    # first order the cards
+    if kCARDS.index(c1) < kCARDS.index(c2):
+        c1, c2 = c2, c1
+    return c1 + c2
+
+def FormatCards(row):
+    card1 = row["card1"].strip("' ")
+    card2 = row["card2"].strip("' ")
+    c1, s1 = card1[0].upper(), card1[1].lower()
+    c2, s2 = card2[0].upper(), card2[1].lower()
+    # first order the cards
+    if kCARDS.index(c1) < kCARDS.index(c2):
+        c1, c2 = c2, c1
+    if s1==s2:
+        return c1+c2+'s'
+    #elif c1 == c2:
+    #    return c1+c2
+    else:
+        return c1+c2+'o'
 
 if __name__ == "__main__":
-    df_raise = pd.read_csv("raise_pl0_pl3.txt")
-    df_fold = pd.read_csv("fold_pl0_pl3.txt")
-    df_call = pd.read_csv("call_pl0_pl3.txt")
-    n_hands = len(df_raise.index) + len(df_fold.index) + len(df_call.index)
-    raise_weights = [1. / float(n_hands)] * len(df_raise.index)
-    fold_weights = [1. / float(n_hands)] * len(df_fold.index)
-    call_weights = [1. / float(n_hands)] * len(df_call.index)
-    PlotHands(df_raise, tag="raise_frequency")
-    PlotHands(df_call, tag="call_frequency")
-    PlotHands(df_fold, tag="fold_frequency")
+    dfr = pd.read_csv("raise_pl0_pl3.txt")
+    dfr['formatted'] = dfr.apply (lambda row: FormatCards(row), axis=1)
+    dfr = pd.DataFrame(dfr.formatted.value_counts().reset_index())
+    dfr.columns = ['cards', 'raise']
+    dfr.set_index('cards', inplace=True, drop=True)
+
+    dfc = pd.read_csv("call_pl0_pl3.txt")
+    dfc['formatted'] = dfc.apply (lambda row: FormatCards(row), axis=1)
+    dfc = pd.DataFrame(dfc.formatted.value_counts().reset_index())
+    dfc.columns = ['cards', 'call']
+    dfc.set_index('cards', inplace=True, drop=True)
+
+    dff = pd.read_csv("fold_pl0_pl3.txt")
+    dff['formatted'] = dff.apply (lambda row: FormatCards(row), axis=1)
+    dff = pd.DataFrame(dff.formatted.value_counts().reset_index())
+    dff.columns = ['cards', 'fold']
+    dff.set_index('cards', inplace=True, drop=True)
+
+    df = pd.concat([dfr, dfc, dff], join='outer', axis=1, sort=False)
+    df.index.name = 'cards'
+    df.fillna(0, inplace=True)
+    #df.sort_index(inplace=True, ascending=False)
+
+    def CalcRaiseWgt(row):
+        return (row['raise']/(row['fold'] + row['call'] + row['raise'])).round(3)
+
+    df['raise_wgt'] = df.apply (lambda row: CalcRaiseWgt(row), axis=1)
+
+    def PlotHands(df):
+        def Parse(x):
+            if x[2] = 'o'
+                return (x[0],x[1],False)
+        parsed_hands = [(x[0],x[1]) for x in df.index]
+        hands_list = [[x[0],x[1]] for x in df.index]
+        print(hands_list)
+        bins_x = bins_y = np.arange(0, len(kCARDS) + 1)
+        #f, ax = plt.subplots(figsize=(10, 10))
+        #h2d = ax.hist2d(x, y, bins=[bins_x, bins_y], cmap=plt.cm.Reds, weights=weights)
+
+    PlotHands(df)
+
+'''
+    hands_list = [
+        x
+        [x.strip(whitespace + "\"'"), y.strip(whitespace + "\"'")]
+        for x, y in zip(df["card1"], df["card2"])
+    ]
+    parsed_hands, n_bad_hands = ParseHands(hands_list)
+    x = [i[0] for i in parsed_hands]
+    x = [kCARDS.index(i) for i in x]
+    y = [i[1] for i in parsed_hands]
+    y = [kCARDS.index(i) for i in y]
+    is_suited = [i[2] for i in parsed_hands]
+
+    bins_x = bins_y = np.arange(0, len(kCARDS) + 1)
+
+    f, ax = plt.subplots(figsize=(10, 10))
+    h2d = ax.hist2d(x, y, bins=[bins_x, bins_y], cmap=plt.cm.Reds, weights=weights)
+    f.colorbar(h2d[3])
+    ax.invert_xaxis()
+    counts = h2d[0]
+    for i in range(counts.shape[0]):
+        for j in range(counts.shape[1]):
+            c = counts[i, j]
+            color = "white" if c > 10 else "black"
+            percent = ""
+            if weights:
+                c = (c * 100.).round(2)
+                color = "white" if c > 0.09 else "black"
+                percent = "%"
+            else:
+                c = int(c)
+            lbl = ""
+            if i > j:
+                lbl = "{0}{1}o\n{2}".format(kCARDS[i], kCARDS[j], c) + percent
+            elif i == j:
+                lbl = "{0}{1}\n{2}".format(kCARDS[i], kCARDS[j], c) + percent
+            else:
+                lbl = "{0}{1}s\n{2}".format(kCARDS[j], kCARDS[i], c) + percent
+            plt.text(
+                bins_x[i] + 0.5,
+                bins_y[j] + 0.5,
+                lbl,
+                ha="center",
+                va="center",
+                color=color,
+            )
+
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+
+    f.suptitle(tag, fontsize=20)
+
+    plt.show()
+    f.savefig("{}.png".format(tag))
+    #values = df['formatted'].value_counts().keys().tolist()
+    #counts = df['formatted'].value_counts().tolist()
+    ##df1 = pd.concat([values, counts], keys=['cards', 'raised'], axis=1)
+    #df1 = df['formatted'].value_counts()#.rename_axis('unique_values').to_frame('counts')
+    #print(df1)
+    #df_fold = pd.read_csv("fold_pl0_pl3.txt")
+    #df_call = pd.read_csv("call_pl0_pl3.txt")
+    #n_hands = len(df_raise.index) + len(df_fold.index) + len(df_call.index)
+    #raise_weights = [1. / float(n_hands)] * len(df_raise.index)
+    #fold_weights = [1. / float(n_hands)] * len(df_fold.index)
+    #call_weights = [1. / float(n_hands)] * len(df_call.index)
+    #PlotHands(df_raise, tag="raise_frequency")
+    #PlotHands(df_call, tag="call_frequency")
+    #PlotHands(df_fold, tag="fold_frequency")
+'''
